98ad1d8aede027346d97bc943ca48700a4524c63
Nick Kou <nick.kou@umbrellar.com>
2017-11-28 15:41:37 +1300

    cleanup

--- regex-metering.py
+++ regex-metering.py
@@ -1,8 +1,13 @@
 import re
+import os
 import json
+import fnmatch
 import collections
 
 # load the data, export the meterId
+# replace the report_target
+report_target = "vm_subFROM2017-11-27TO2017-11-28Hourly"
+
 output = []
 def export_meterid(file_name):
     with open(file_name,'r') as file:
@@ -10,28 +15,14 @@ def export_meterid(file_name):
         for f in file_json:
             output.append(f['properties']['meterId'])
 
-export_meterid('vm_subFROM2017-11-27TO2017-11-28Hourly.txt')
-export_meterid('vm_subFROM2017-11-27TO2017-11-28Hourly_Page_2.txt')
-export_meterid('vm_subFROM2017-11-27TO2017-11-28Hourly_Page_3.txt')
+report_patten = report_target + "*"
+
+for file in os.listdir('.'):
+    if fnmatch.fnmatch(file, report_patten):
+        export_meterid(file)
 
 no_duplicate_result = (set([item for item in output if output.count(item) >1 ]))
 for n in no_duplicate_result:
     print(n)
 
-# output = []
-# with open('vm_subFROM2017-11-27TO2017-11-28Hourly.txt','r+') as file:
-#     file_json = json.load(file)
-#     #print(file_json['value'])
-#     for f in file_json:
-#         output.append(f['properties']['meterId'])
-# #    print(output)
-#
-# # remove duplicate meterId
-# no_duplicate_result = (set([item for item in output if output.count(item) >1 ]))
-# for n in no_duplicate_result:
-#     print(n)
-
-# "meterId": "09F8879E-87E9-4305-A572-4B7BE209F857",
-
-
-# 'vm_subFROM2017-11-27TO2017-11-28Hourly.txt'
+print("There are " + str(len(no_duplicate_result)) + " meter ids available." )
